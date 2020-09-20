ae8f28e9f2064cd8cd4f94374410c636d6a58e3d
Raymond Law <raymondlaw@users.noreply.github.com>
2020-09-20 00:28:34 -0400

    Update mistake.js

--- appendix/mistake.js
+++ appendix/mistake.js
@@ -4,6 +4,10 @@ fs.readFile("input/file02.txt","utf8", (err, data) => {
     fileContents = data;
 });
 
+/*
+console.log(`Finished Reading File: `, fileContents?.length);
+*/
+
 setTimeout(
     function(){
         console.log(`Finished Reading File: `, fileContents?.length);
