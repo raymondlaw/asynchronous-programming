8f1d45c4585719f969d1ace36c2de127b39e4cb4
Raymond Law <raymondlaw@users.noreply.github.com>
2020-09-21 04:32:48 -0400

    Update 01-concurrent-to-synchronous.js

--- 11-concurrent-to-synchronous/01-concurrent-to-synchronous.js
+++ 11-concurrent-to-synchronous/01-concurrent-to-synchronous.js
@@ -4,18 +4,18 @@ const ip_addresses = [];
 let count = 0;
 
 for(let i = 0; i < domains.length; i++){
-	resolve(domains[i]);
+    resolve(domains[i]);
 }
 function resolve(domain){
     dns.resolve(domain, after_resolution);
-	function after_resolution(err, records){
-		ip_addresses[count] = records;
-		count++;
-		if(count === domains.length){
-			next_task(ip_addresses.flat().join("\r\n"));
-		}
-	}
+    function after_resolution(err, records){
+        ip_addresses[count] = records;
+        count++;
+        if(count === domains.length){
+            next_task(ip_addresses.flat().join("\r\n"));
+        }
+    }
 }
 function next_task(str){
-	console.log(str);
+    console.log(str);
 }
